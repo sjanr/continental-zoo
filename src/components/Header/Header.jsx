1b35004debeab69a2c67a352226b207e2d1b202f
sjanr <suratosrjan@gmail.com>
2023-08-29 14:10:54 -0700

    Added logo and nav bar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import "./Header.scss";
+import { NavLink, Link } from "react-router-dom";
+import headerImg from "../../assets/logo/continentalZooLogo.svg";
+
+function Header() {
+  return (
+    <>
+      <header className="header">
+        <div className="header__logo"></div>
+        <div className="header__nav">
+          <div className="header__nav-item">TICKETS & MEMBERSHIPS</div>
+          <div className="header__nav-item">PLAN YOUR VISIT</div>
+          <div className="header__nav-item">EXHIBITS & EXPERIENCES</div>
+          <div className="header__nav-item">GROUPS</div>
+          <div className="header__nav-item buy-ticket">BUY A TICKET</div>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
