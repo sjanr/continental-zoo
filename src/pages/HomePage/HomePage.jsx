a08c3ed6bf7f86073744be50cc8057eb428f0501
sjanr <suratosrjan@gmail.com>
2023-08-30 11:37:46 -0700

    Created info item sections

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -50,6 +50,51 @@ function HomePage() {
           </div>
         </div>
       </section>
+      <section className="info">
+        <div className="info__item">
+          <div className="info__item-image-koala"></div>
+          <div className="info__item-text-container koala-container">
+            <div className="info__item-title">Exemplary Animal Welfare</div>
+            <div className="info__item-text">
+              A dedicated care team comprising professional animal behaviorists,
+              zoologists, veterinarians, and dieticians ensures the highest
+              standards of animal well-being. Our extensive experience in
+              tending to a diverse range of creatures over many years forms the
+              foundation of our successful mission, encompassing rescue,
+              rehabilitation, and reintegration efforts.
+            </div>
+          </div>
+        </div>
+        <div className="info__item">
+          <div className="info__item-text-container">
+            <div className="info__item-title">Globally Acknowledged</div>
+            <div className="info__item-text">
+              Continental Zoo stands as a global icon of display and
+              interpretation excellence. Pioneering the integration of
+              professional naturalists and interpretive specialists throughout
+              its habitats, it offers unprecedented insight into animal
+              behaviors.
+            </div>
+          </div>
+          <div className="info__item-image-peacock"></div>
+        </div>
+        <div className="info__item">
+          <div className="info__item-image-crocodile"></div>
+          <div className="info__item-text-container">
+            <div className="info__item-title">
+              Over 40,000 Rescues and Growing
+            </div>
+            <div className="info__item-text">
+              With round-the-clock availability, our devoted team of animal
+              care, veterinary, and animal rescue specialists stands ready to
+              respond, every single day of the year. Our dedication to
+              safeguarding these remarkable creatures and their environments
+              underscores our commitment to you: a visit to Continental Zoo is a
+              visit that resonates with purpose.
+            </div>
+          </div>
+        </div>
+      </section>
       <section className="description">
         <div className="description__text--header">
           THRILLED TO EXPLORE WILDLIFE WITH YOU
