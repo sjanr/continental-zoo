7d7b6c6328063f4705cc1dcc2e68826d2e3f1973
sjanr <suratosrjan@gmail.com>
2023-09-07 15:49:00 -0700

    Created footer template, and added social media icon SVG

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { NavLink, Link } from "react-router-dom";
 function Footer() {
   return (
     <>
-      <div className="footer__container">
+      <div className="footer__section">
         <div className="footer">
           <div className="footer__container">
             <div className="footer__container-item">
