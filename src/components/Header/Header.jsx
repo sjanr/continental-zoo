539ffe7095e8941ba4e5215dab5f05240d300e84
sjanr <suratosrjan@gmail.com>
2023-09-08 12:20:23 -0700

    Added border bottom when hovering on nav item

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,11 +7,13 @@ function Header() {
       <header className="header">
         <div className="header__logo"></div>
         <div className="header__nav">
-          <div className="header__nav-item">TICKETS & MEMBERSHIPS</div>
-          <div className="header__nav-item">PLAN YOUR VISIT</div>
-          <div className="header__nav-item">EXHIBITS & EXPERIENCES</div>
-          <div className="header__nav-item">GROUPS</div>
-          <div className="header__nav-item buy-ticket">BUY A TICKET</div>
+          <div className="header__nav-item dropdown">TICKETS & MEMBERSHIPS</div>
+          <div className="header__nav-item dropdown">PLAN YOUR VISIT</div>
+          <div className="header__nav-item dropdown">
+            EXHIBITS & EXPERIENCES
+          </div>
+          <div className="header__nav-item dropdown">GROUPS</div>
+          <div className="buy-ticket">BUY A TICKET</div>
         </div>
       </header>
     </>
